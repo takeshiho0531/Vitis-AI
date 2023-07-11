64890b44fab4e4e0378e0f7b5764f43206f2c986
takeshiho0531 <kawadatakeshiho@gmail.com>
2023-07-11 11:36:25 +0900

    feat:update docker_run

--- docker_run.sh
+++ docker_run.sh
@@ -78,7 +78,7 @@ docker_run_params=$(cat <<-END
     -v /opt/xilinx/overlaybins:/opt/xilinx/overlaybins \
     -e USER=$user -e UID=$uid -e GID=$gid \
     -v $DOCKER_RUN_DIR:/vitis_ai_home \
-    -v $HERE:/workspace \
+    -v ../../Chipathon:/workspace \
     -w /workspace \
     --rm \
     --network=host \
